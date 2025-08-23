441a7f74c70ee21b2565f8386dd6be50a5340b46
19991126kai <19991126kai@gmail.com>
2025-08-23 07:02:33 +0000

    カテゴリのnew, create実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     devise_for :users
     get "/timer", to: "timers#show"
-    resources :categories, only: [:index]
+    resources :categories, only: [:index, :new, :create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
