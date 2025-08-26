84e648335057069e0bbd5d8394177c537edeaac4
19991126kai <19991126kai@gmail.com>
2025-08-26 13:50:27 +0000

    一旦ビューだけ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  get "/timer", to: "timers#show"
+  resource :timer, only: [ :show ]
   resources :categories, except: [ :show ]
   resources :logs, except: [ :show ]
 
