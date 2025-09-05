f75eb6ec076fa2eef2c0b724c0da0ccda5584b39
19991126kai <19991126kai@gmail.com>
2025-09-05 03:26:32 +0000

    シェア画面UIを作成

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,4 +1,13 @@
 class SharesController < ApplicationController
   def show
+    @categories = current_user.categories.order(id: "desc")
+
+    # 学習時間算出用パラメータ
+    @date = if params[:date].present?
+      Date.parse(params[:date]) rescue Date.current
+    else
+      Date.current
+    end
+    @category_id = params[:category_id].presence || @categories.first&.id
   end
 end
