bc3641d9a6d6a9383100eba4f0f9e30754b13286
19991126kai <19991126kai@gmail.com>
2025-09-08 11:48:09 +0000

    ゲストログアウトでアカウント削除されないバグを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in", as: :guest_sign_in
   end
