3cd2dc939e909a28d88028db19217f9cde06243f
19991126kai <19991126kai@gmail.com>
2025-10-31 11:43:32 +0000

    Timer#showをstudy_dateを使うように修正

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -2,15 +2,14 @@ class TimersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @log = current_user.logs.new
+    @log = current_user.logs.build
     @category = current_user.categories.find_by(id: params[:category_id])
 
     if @category
       @logs = current_user.logs
                         .where(category_id: @category.id)
-                        .order(start_time: :desc)
-      @today_total_duration = @logs.where(start_time: Time.current.beginning_of_day..Time.current.end_of_day)
-                                   .sum(:duration)
+                        .order(study_date: :desc)
+      @today_total_duration = @logs.where(study_date: Date.current).sum(:duration)
       @total_duration = @logs.sum(:duration)
     else
       redirect_to root_path, alert: "無効なカテゴリです" and return
