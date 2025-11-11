7ddd7ef12bcbd3377b5ba53940686c31f5158e3c
19991126kai <19991126kai@gmail.com>
2025-11-11 01:37:54 +0000

    ログ一覧にkaminariでページネーションを追加

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -5,15 +5,18 @@ class TimersController < ApplicationController
     @log = current_user.logs.build
     @category = current_user.categories.find_by(id: params[:category_id])
 
-    if @category
-      @logs = current_user.logs
-                        .where(category_id: @category.id)
-                        .order(study_date: :desc)
-                        .order(id: :desc)
-      @today_total_duration = @logs.where(study_date: Date.current).sum(:duration)
-      @total_duration = @logs.sum(:duration)
-    else
+    unless @category
       redirect_to root_path, alert: "無効なカテゴリです" and return
     end
+
+    # 何回もDBにアクセスしないように共通化
+    base_logs = current_user.logs
+                             .where(category_id: @category.id)
+                             .order(study_date: :desc, id: :desc)
+
+    @today_total_duration = base_logs.where(study_date: Date.current).sum(:duration)
+    @total_duration       = base_logs.sum(:duration)
+
+    @logs = base_logs.page(params[:page]).per(10)
   end
 end
